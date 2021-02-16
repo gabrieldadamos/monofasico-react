cac7f1e82049b4462fcb7e8d4cb324b9c525ef88
gabrieldadamos <gabrieldadamos@hotmail.com>
2021-02-15 22:33:57 -0300

    master

---
+++ build/precache-manifest.e282735312a8cf338b0d92a6e32d4a23.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00a999e542f6b67e7f23b475da8a0ebe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3307b91b721569b9ef98",
+    "url": "/static/css/2.18fb2ca0.chunk.css"
+  },
+  {
+    "revision": "3307b91b721569b9ef98",
+    "url": "/static/js/2.0d5433ca.chunk.js"
+  },
+  {
+    "revision": "289cfd8206daa7d55403cf92103aaeb2",
+    "url": "/static/js/2.0d5433ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b5ad5b781a1eb037671",
+    "url": "/static/js/main.351ae494.chunk.js"
+  },
+  {
+    "revision": "02abd13eaf1d7c33000d",
+    "url": "/static/js/runtime-main.6a771f16.js"
+  },
+  {
+    "revision": "09c45a559b3704d548af3837d2dda64e",
+    "url": "/static/media/nota.09c45a55.xml"
+  }
+]);
